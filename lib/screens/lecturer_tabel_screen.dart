be7461442f2878897678c872208ee4202b9fe454
Mansour <mansourzaki2000@gmail.com>
2022-06-06 17:47:08 +0300

    show tabel of each lecturer

---
+++ lib/screens/lecturer_tabel_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:timetabling/models/subject.dart';
+
+class LecturereTableScreen extends StatefulWidget {
+  const LecturereTableScreen(
+      {Key? key, required this.lecturer, required this.allSubjects})
+      : super(key: key);
+  final String lecturer;
+  final List<Subject> allSubjects;
+  @override
+  State<LecturereTableScreen> createState() => _LecturereTableScreenState();
+}
+
+class _LecturereTableScreenState extends State<LecturereTableScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('${widget.lecturer} Tabel')),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          
+          children: [
+            DataTable(
+              border: TableBorder.all(),
+              columns: const [
+                DataColumn(
+                  label: Text("Subject"),
+                ),
+                DataColumn(
+                  label: Text("Groups"),
+                ),
+                DataColumn(
+                  label: Text("Assigned Classes"),
+                ),
+                DataColumn(
+                  label: Text("Days"),
+                ),
+                DataColumn(label: Text('Time')),
+              ],
+              rows: widget.allSubjects
+                  .where((element) => element.lecturer == widget.lecturer)
+                  .map(
+                    (e) => DataRow(cells: [
+                      DataCell(Container(
+                        width: 200,
+                        child: Text(
+                          e.subject,
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      )),
+                      DataCell(Container(
+                        width: 100,
+                        child: Text(
+                          e.group.toString(),
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      )),
+                      DataCell(Container(
+                        width: 100,
+                        child: Text(
+                          e.assignedClassroom,
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      )),
+                      DataCell(Container(
+                        width: 200,
+                        child: Text(
+                          e.getDays.toString(),
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      )),
+                      DataCell(Container(
+                        width: 100,
+                        child: Text(
+                          e.getTime,
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      )),
+                    ]),
+                  )
+                  .toList(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/lecturers_screen.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../models/subject.dart';
+import 'lecturer_tabel_screen.dart';
+
+class LecturersScreen extends StatefulWidget {
+  const LecturersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LecturersScreen> createState() => _LecturersScreenState();
+}
+
+class _LecturersScreenState extends State<LecturersScreen> {
+  List<String> allNames = [];
+  List<Subject> allSubjects = [];
+  List<String> names = [];
+  List<DataRow> rows = [];
+  @override
+  void initState() {
+    loadSubjects();
+    getAllLecturers();
+    super.initState();
+  }
+
+  void getAllLecturers() {
+    List<String> names = [];
+    allSubjects.forEach((element) {
+      names.add(element.lecturer);
+    });
+
+    allNames = names.toSet().toList();
+  }
+
+  Future loadSubjects() async {
+    final jsonString = await rootBundle.loadString('assets/iug_output.json');
+    setState(() {
+      List<dynamic> subjectsList = jsonDecode(jsonString);
+      allSubjects = subjectsList
+          .map(
+            (json) => Subject.fromJson(json),
+          )
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getAllLecturers();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Lecturer Table')),
+      body: Container(
+        child: ListView.builder(
+            itemCount: allNames.length,
+            itemBuilder: (context, i) {
+              return ListTile(
+                  title: Text(allNames[i]),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return LecturereTableScreen(
+                        lecturer: allNames[i],
+                        allSubjects: allSubjects,
+                      );
+                    }));
+                  });
+            }),
+      ),
+    );
+  }
+}
